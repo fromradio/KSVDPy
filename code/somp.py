170613fd78d50b2102394b022295fb51aad58e78
fromradio <ruimin.wang13@gmail.com>
2014-04-09 18:11:02 +0800

    somp finished

--- code/somp.py
+++ code/somp.py
@@ -2,7 +2,8 @@
 # omp algorithm for a single vector
 #! /usr/bin/env python
 
-import numpy as numpy
+import numpy as np
+import scipy.linalg as linalg
 
 def somp(vec,dic,sp = 5):
 	"""
@@ -10,7 +11,12 @@ def somp(vec,dic,sp = 5):
 	input:
 		vec -- the input vector
 		dic -- the dictionary
-	basic assumptions:
+	return:
+		a tuple (chosen,alpha,residual)
+		'chosen' is the chosen indices of the corresponding atom functions
+		'alpha' is the corresponding coordinate
+		'residual' is the residual of the fitting
+	basic assumptions
 		the dictionary is supposed to be normalized before running the function
 	"""
 	# vec = dic*alpha
@@ -18,14 +24,38 @@ def somp(vec,dic,sp = 5):
 	dim = dic.shape[1]
 	# in case that dim is smaller than sp
 	n = min(dim,sp)
-
+	# initialization
+	residual = vec 		# residual
+	notchosen = range(0,dim)
+	chosen = []
 	for i in range(0,n):
 		# find the corresponding vectors
-		pass
+		# first sweeping:
+		e = np.dot(dic.T[notchosen,:],vec)
+		e = np.abs(e)
+		# support update
+		ind = notchosen[e.argmax()]
+		notchosen.remove(ind)
+		chosen.append(ind)
+		# provision
+		A = dic[:,chosen]
+		r = linalg.lstsq(A,vec)
+		alpha = r[0]
+		residual = r[1]
+	return (chosen,alpha,residual)
 	
+"""
+main function is used for test
+"""
 def main():
-	dic = np.array([[1,0],[0,1]])
-	vec = np.array([1,0])
+	#dic = np.array([[1,0,0],[0,1,0],[0,0,1]])
+	dic = np.random.rand(100,200)
+	dic /= np.linalg.norm(dic,axis = 0 )
+	#print "norm of dic is ",np.linalg.norm(dic,axis=0)
+	#vec = np.array([1,0,1])
+	vec = np.random.rand(100)
+	a = somp(vec,dic,99)
+	print a
 
 if __name__ == '__main__':
 	main()
