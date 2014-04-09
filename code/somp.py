53374981e868f83d0c053158d8097e0a9b8c6368
fromradio <ruimin.wang13@gmail.com>
2014-04-09 16:21:05 +0800

    code modified

--- code/somp.py
+++ code/somp.py
@@ -1,2 +1,31 @@
 # single omp algorithm
+# omp algorithm for a single vector
 #! /usr/bin/env python
+
+import numpy as numpy
+
+def somp(vec,dic,sp = 5):
+	"""
+	omp algorithm for a single vector
+	input:
+		vec -- the input vector
+		dic -- the dictionary
+	basic assumptions:
+		the dictionary is supposed to be normalized before running the function
+	"""
+	# vec = dic*alpha
+	# alpha is a sparse coordinate
+	dim = dic.shape[1]
+	# in case that dim is smaller than sp
+	n = min(dim,sp)
+
+	for i in range(0,n):
+		# find the corresponding vectors
+		pass
+	
+def main():
+	dic = np.array([[1,0],[0,1]])
+	vec = np.array([1,0])
+
+if __name__ == '__main__':
+	main()
